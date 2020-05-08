4415cd9876cfceb5219360f343156eb39e76b09a
TristenKGM <tristenkreutz@gmail.com>
2020-05-08 00:10:43 -0600

    Movement, Obstacles, Collision
    Modified movement speeds.
    Added obstacles for testing.
    Partially implemented collision detection.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float thrustSpeed = 1000f;
+    [SerializeField] float rotationSpeed = 100f;
+
     private Rigidbody rb;
     private AudioSource audioSource;
 
@@ -18,14 +21,44 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessThrusting();
+        ProcessRotations();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Safe":
+                print("Safe");
+                break;
+
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
+    private void ProcessThrusting()
+    {
+        float thrustPerFrame = thrustSpeed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);
+            rb.AddRelativeForce(Vector3.up * thrustPerFrame);
+            ThrustSound(true);
+        }
+
+        else
+        {
+            ThrustSound(false);
+        }
+    }
+
+    private void ThrustSound(bool thrusting)
+    {
+        if (thrusting)
+        {
             audioSource.volume = 0.5f;
 
             if (!audioSource.isPlaying)
@@ -46,15 +79,23 @@ public class Rocket : MonoBehaviour
                 audioSource.Stop();
             }
         }
+    }
+
+    private void ProcessRotations()
+    {
+        rb.freezeRotation = true; // Gives manual control to rotations
+        float rotationPerFrame = rotationSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationPerFrame);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationPerFrame);
         }
+
+        rb.freezeRotation = false; // Returns control of rotations to physics engine
     }
 }
